b3114973b9ef3b6ff7bb8fa95b507f85e75ed22a
Roger Tan <teochewthunder@gmail.com>
2023-10-19 22:21:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,43 @@
 <?php
-	$name = (isset($_GET["name"]) ? $_GET["name"] : "Human Being");
+    $name = (isset($_GET["name"]) ? $_GET["name"] : "Human Being");
 
-	$themes = [
+    $themes = [
 		["name" => "snow", "title" => "Seasons Greetings for a White Christmas, " . $name . "!", "prompt" => "Generate a short paragraph, between 80 to 100 words, as a greeting on a Christmas Card, involving snow and winter"],
 		["name" => "food", "title" => "Dear " . $name . ", wishing you festive food and fun!", "prompt" => "Generate a short poem, using less than 100 words, as a greeting on a Christmas Card, revoving around food"],
 		["name" => "nativity", "title" => "Have a blessed Christmas, " . $name . "!", "prompt" => "Generate a short paragraph, between 50 to 100 words, as a greeting on a Christmas Card, involving Jesus and the Nativity"]
-	];
+    ];
 
-	$index = rand(0, 2);
+    $index = rand(0, 2);
+    $content = $themes[$index]["prompt"];
 
-	$content = $themes[$index]["prompt"];
-
-	//api call
-	$key = "sk-xxx";
-	
+    $key = "sk-xx";
+    $org = "org-FUOhDblZb1pxvaY6YylF54gl";
     $url = 'https://api.openai.com/v1/chat/completions';  
     
-    $headers = array(
-        "Authorization: Bearer {$key}",
-        "OpenAI-Organization: org-FUOhDblZb1pxvaY6YylF54gl", 
+    $headers = [
+        "Authorization: Bearer " . $key,
+        "OpenAI-Organization: " . $org, 
         "Content-Type: application/json"
-    );
+    ];
     
-    // Define messages
     $messages = [];
-	$obj = [];
+    $obj = [];
     $obj["role"] = "user";
     $obj["content"] = $themes[$index]["prompt"];
-	$messages[] = $obj;
+   $messages[] = $obj;
     	
-    // Define data
-    $data = array();
+    $data = [];
     $data["model"] = "gpt-3.5-turbo";
     $data["messages"] = $messages;
     $data["max_tokens"] = 1000;
 
-    // init curl
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-	$result = curl_exec($curl);
+    $result = curl_exec($curl);
     if (curl_errno($curl)) {
         echo 'Error:' . curl_error($curl);
     } 
